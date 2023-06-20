fbcd525c9f1cda98da987e84ac2c864bb1c64198
0xhatsume <0xhatsume@gmail.com>
2023-06-20 17:31:09 +0800

    build: update with server watch for wsl hot reload

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    watch: {
+      usePolling: true,
+    },
     port: 3000,
     fs: {
       strict: false,
