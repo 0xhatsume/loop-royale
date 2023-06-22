4224438e043a9933c759d92aac52b5e3f4b014e4
0xhatsume <0xhatsume@gmail.com>
2023-06-22 12:38:32 +0800

    feat: add wagmi connect function in wallet button

--- packages/client/src/components/ConnectWallet/index.tsx
+++ packages/client/src/components/ConnectWallet/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import { useAccount, useConnect, useEnsName, useDisconnect } from 'wagmi';
+import { InjectedConnector } from 'wagmi/connectors/injected'
 
 const ConnectWallet = () => {
+    const { address, isConnected } = useAccount()
+    const { data: ensName } = useEnsName({ address })
+    const { connect } = useConnect({
+        connector: new InjectedConnector(),
+    })
+    const { disconnect } = useDisconnect()
+
+    const shortAddress = address?.length > 40 ? 
+        address.slice(0, 5) + "..." + address.slice(-4) : (address??"");
+
+    const handleConnect = () => {
+        !isConnected ? connect() : disconnect();
+    }
+
     return (
         <button className="
     px-4 py-2 border rounded-lg
     bg-orange-500 hover:bg-orange-700
     "
-        >Connect Wallet</button>
+            onClick={() => handleConnect()}
+        >{isConnected ? (ensName ?? shortAddress) : "Connect Wallet"}</button>
     )
 }
 
--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -8,16 +8,36 @@ import { setup } from "./mud/setup";
 import { MUDProvider } from "./MUDContext";
 import { ToastContainer } from "react-toastify";
 
+import { WagmiConfig, createConfig, configureChains } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { bladedao } from "./mud/supportedChains";
+
 const rootElement = document.getElementById("react-root");
 if (!rootElement) throw new Error("React root not found");
 const root = ReactDOM.createRoot(rootElement);
 
+
 // TODO: figure out if we actually want this to be async or if we should render something else in the meantime
 setup().then((result) => {
+  // console.log("setup complete")
+  // console.log(result)
+
+  const { chains, publicClient, webSocketPublicClient } = configureChains(
+    [bladedao],
+    [publicProvider()],
+  )
+  const config = createConfig({
+    autoConnect: true,
+    publicClient,
+    webSocketPublicClient,
+  })
+
   root.render(
     <MUDProvider value={result}>
+      <WagmiConfig config={config}>
       <App />
       <ToastContainer position="bottom-right" draggable={false} theme="dark" />
+      </WagmiConfig>
     </MUDProvider>
   );
   mountDevTools();
