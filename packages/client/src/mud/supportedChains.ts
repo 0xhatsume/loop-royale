a4e2baf7cb0b1adddfdcdb5873dd312bad6d447f
0xhatsume <0xhatsume@gmail.com>
2023-06-22 10:44:48 +0800

    feat: add blade chain

--- packages/client/src/mud/supportedChains.ts
+++ packages/client/src/mud/supportedChains.ts
@@ -1,5 +1,31 @@
 import { MUDChain, latticeTestnet } from "@latticexyz/common/chains";
-import { foundry } from "@wagmi/chains";
+import { foundry, Chain } from "@wagmi/chains";
+
+export const bladedao = {
+    id: 1011365,
+    name: 'BladeDAO',
+    network: 'bladedao',
+    nativeCurrency: {
+        decimals: 18,
+        name: 'Blade',
+        symbol: 'BLADE',
+    },
+    rpcUrls: {
+      default: {
+        http: ['https://flashlayer.alt.technology/bladedao74eb1498/rpc'],
+        webSocket: ['wss://flashlayer.alt.technology/bladedao74eb1498/ws'],
+      },
+      public: {
+        http: ['https://flashlayer.alt.technology/bladedao74eb1498/rpc'],
+        webSocket: ['wss://flashlayer.alt.technology/bladedao74eb1498/ws'],
+      },
+    },
+
+    blockExplorers: {
+        default: { name: 'Bladescan', url: 'https://explorer.alt.technology?rpcUrl=https://flashlayer.alt.technology/bladedao74eb1498/rpc' },
+    },
+
+} as const satisfies Chain
 
 // If you are deploying to chains other than anvil or Lattice testnet, add them here
-export const supportedChains: MUDChain[] = [foundry, latticeTestnet];
+export const supportedChains: MUDChain[] = [bladedao, foundry, latticeTestnet];
