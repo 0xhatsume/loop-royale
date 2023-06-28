3d0a1ec1298a159b548a6d1d922df26b73165485
0xhatsume <0xhatsume@gmail.com>
2023-06-28 09:08:07 +0800

    fix: update move system to reflect short term burner wallet fix

--- packages/client/src/hooks/useGameKeyListener.ts
+++ packages/client/src/hooks/useGameKeyListener.ts
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
 import { useMUD } from "../MUDContext";
+import { useAccount } from "wagmi";
 
 export const useGameKeyListener = (mapId: string) => {
     // assume mapId is padded to 32 bytes
     //const mapId = '0x0000000000000000000000000000000000000000000000000000000000000001'
+    const { account } = useAccount();
     const {
         systemCalls: { moveBy },
     } = useMUD();
@@ -12,19 +14,19 @@ export const useGameKeyListener = (mapId: string) => {
         const listener = (e: KeyboardEvent) => {
         if (e.key === "ArrowUp") {
             //console.log("up");
-            moveBy(mapId, 0, -1);
+            moveBy(mapId, 0, -1, account);
         }
         if (e.key === "ArrowDown") {
             //console.log("down");
-            moveBy(mapId, 0, 1);
+            moveBy(mapId, 0, 1, account);
         }
         if (e.key === "ArrowLeft") {
             //console.log("left");
-            moveBy(mapId, -1, 0);
+            moveBy(mapId, -1, 0, account);
         }
         if (e.key === "ArrowRight") {
             //console.log("right");
-            moveBy(mapId, 1, 0);
+            moveBy(mapId, 1, 0, account);
         }
         };
 
